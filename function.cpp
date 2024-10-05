8f6103828cb9a2a6cc68612cd7b1bf0d50d33ee3
miselee <miseleaisya@gmail.com>
2024-10-05 23:01:19 +0700

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void Menu(){
+cout << "Pilihan yang tersedia: " << endl;
+cout << "1. Gunting" << endl;
+cout << "2. Batu" << endl;
+cout << "3. Kertas" << endl;
+}
+
 int main (){
+  int pemain1, pemain2, pilihan_pemain1, pilihan_pemain2;
+  cout << "==========Permainan Dimulai==========" << endl << endl;
+  cout << "Pemain 1, tentukan pilihan anda!" << endl;
+  Menu():
+  cout << "Masukan pilihan: ";
+  cin >> pilihan_pemain1;
+  switch(pilihan_pemain1){
+    case 1 : pilihan_pemain1 = 1;
+      break;
+    case 2 : pilihan_pemain1 = 2;
+      break;
+    case 3 : pilihan_pemain1 = 3;
+      break;
+    default: cout << "plihan tidak tersedia" << endl;
+      return 0;
+  } cout << endl;
+      
 return 0;
 }
